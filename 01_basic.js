ac30824e4e17da394935a9e22d73835730fac1e2
RAHUL <rahulmeetawa@gmail.com>
2023-02-19 12:46:31 +0530

    TS

--- 01_basic.js
+++ 01_basic.js
@@ -1,1 +1,13 @@
-console.log("rahulsaini");
+/*
+  TODO:
+  1.create a ts file which extension name .ts which stand for typescipt
+  2.inside ts file add syntax according to you
+  3.then go to terminal
+  4.write tsc ts_filename which you make
+        A.using tsc(typescript compiler) compiler which convert ts into a js whcih is complete provide all oops concept in javascipt
+  5.then we show a file which extension name is js and this js file name is similiar to ts
+  6.After all that we can add this js file into script tag in html file
+*/
+var a = "rahul";
+console.warn(a);
+document.write(a);
