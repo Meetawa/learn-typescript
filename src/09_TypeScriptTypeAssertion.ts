89a6032998429c34b98ff80fec539f8083ee700f
RAHUL <rahulmeetawa@gmail.com>
2023-02-21 18:36:46 +0530

    TS

---
+++ src/09_TypeScriptTypeAssertion.ts
@@ -0,0 +1,27 @@
+// Type assertion is a technique that informs the compiler about the type of a variable.
+
+/* 
+Example:
+    let x;
+    x="rahulsaini";
+    console.log(x.length); //we can't do this because it gives a any type so x is no idea about .length
+*/
+
+/* 
+    Two Method in Type assertion:
+        - <> angular brackets
+        - as keyward 
+*/
+
+
+//Method 1:<> bracket
+let x;
+x="rahulsaini";
+console.log((<string>x).length);
+
+//Method 2: as keywords
+console.log((x as string).length);
+
+
+
+
